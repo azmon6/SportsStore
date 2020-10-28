ca326aa330c2f7581230417208cdf27d74f3b500
kaloqn todorkov <kaloqn_bss@abv.bg>
2020-10-28 14:35:24 +0200

    Add project files.

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+    }
+}
